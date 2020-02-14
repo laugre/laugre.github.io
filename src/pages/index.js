f5b69a7096560bda1b8da6a1c94c8437d87c40ce
laugre <laurent@blueyeti.fr>
2020-02-14 18:49:21 +0100

    add prestations section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
-import Typewriter from 'typewriter-effect';
 import ScrollLock from 'react-scrolllock';
-
+import Typewriter from 'typewriter-effect';
+import config from '../../config';
+import Footer from '../components/Footer';
+import Gallery from '../components/Gallery';
 import Layout from '../components/Layout';
 import Projects from '../components/Projects';
-import Footer from '../components/Footer';
 import Scroll from '../components/Scroll';
-import Gallery from '../components/Gallery';
-import config from '../../config';
 
 const ROW1_IMAGES = [
   {
@@ -24,7 +23,7 @@ const ROW1_IMAGES = [
   },
   {
     src: require('../assets/images/skills/sass-seeklogo.com.svg'),
-    caption: "Sass",
+    caption: 'Sass',
   },
 ];
 
@@ -39,11 +38,11 @@ const ROW2_IMAGES = [
   },
   {
     src: require('../assets/images/skills/vuejs-seeklogo.com.svg'),
-    caption: "Vue",
+    caption: 'Vue',
   },
   {
     src: require('../assets/images/skills/git-seeklogo.com.svg'),
-    caption: "Git",
+    caption: 'Git',
   },
 ];
 
@@ -90,11 +89,11 @@ const IndexPage = () => {
       .pauseFor(pause)
       .typeString(
         '<p><strong>Activité</strong>, <strong>Réactivité</strong> et <strong>Interactivité</strong> sont les trois critères qui guident mes développements.</p>'
-      )
-      // .pauseFor(pause * 2)
-      // .callFunction(() => {
-      //   setShowProjectButton(true);
-      // });
+      );
+    // .pauseFor(pause * 2)
+    // .callFunction(() => {
+    //   setShowProjectButton(true);
+    // });
   };
 
   return (
@@ -136,7 +135,27 @@ const IndexPage = () => {
 
       <article className="container box style2">
         <header>
-          <h2>Compétences</h2>
+          <h2>Prestations</h2>
+          <p>
+            Afin de développer votre communication digitale et gagner en
+            visibilité sur le web je propose de vous accompagner dans la
+            réalisation de votre projet.
+          </p>
+          <p>
+            <strong>site vitrine</strong> | <strong>site one page</strong> | <strong>landing page</strong> | <strong>e-commerce</strong> | <strong>sur
+            mesure</strong>
+          </p>
+          <p>
+            À l'ère à laquelle les sites web sont largement plus consultés
+            depuis des mobiles (smartphones et tablettes), il devient
+            indispensable de s'adapter à tous les formats d'affichage.
+          </p>
+          <p><strong>rafraîchissement de site</strong> | <strong>refonte</strong> | <strong>application mobile dédiée</strong></p>
+          <p>
+            En constante évolution, une veille quotidienne est primordiale pour
+            suivre et s'inspirer des nouveaux concepts du web
+          </p>
+          <p><strong>responsive design</strong> | <strong>creative coding</strong> | <strong>UX design</strong> | <strong>interactivity</strong> | <strong>progressive web app</strong></p>
         </header>
 
         <div className="inner gallery">
