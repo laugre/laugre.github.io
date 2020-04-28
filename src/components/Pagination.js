2bd1cd57e67a1a59c7516cf6c5d7784943174bd2
laugre <laurent@blueyeti.fr>
2020-04-28 14:29:43 +0200

    remove arrow icons underline

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,18 +5,8 @@ import '../assets/sass/pagination.scss';
 export default function Pagination({ previous, next }) {
   return (
     <div className="pagination-buttons">
-      <AniLink cover to={previous} direction="right" bg="black">
-        <div
-          className="icon fa-arrow-left button-previous"
-          // style={{ fontSize: 1.5 + 'em' }}
-        ></div>
-      </AniLink>
-      <AniLink cover to={next} direction="left" bg="black">
-        <div
-          className="icon fa-arrow-right button-next"
-          // style={{ fontSize: 1.5 + 'em' }}
-        ></div>
-      </AniLink>
+      <AniLink cover to={previous} direction="right" bg="black" className="icon fa-arrow-left button-previous" />
+      <AniLink cover to={next} direction="left" bg="black" className="icon fa-arrow-right button-next" />
     </div>
   );
 }
